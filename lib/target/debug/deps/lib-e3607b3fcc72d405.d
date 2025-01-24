d8e2568c8290a9bfbb892ef23002a0557f48d8cd
@Elephant9748 <adytamasurya@protonmail.com>
2025-01-24 17:13:35 +0700

    .gitignore: /lib/target

--- lib/target/debug/deps/lib-e3607b3fcc72d405.d
+++
@@ -1,5 +0,0 @@
-/home/rigel/project/paper_backup/lib/target/debug/deps/liblib-e3607b3fcc72d405.rmeta: src/lib.rs
-
-/home/rigel/project/paper_backup/lib/target/debug/deps/lib-e3607b3fcc72d405.d: src/lib.rs
-
-src/lib.rs:
--- lib/target/debug/deps/lib-eaabc3bd1e34bc4e.d
+++
@@ -1,5 +0,0 @@
-/home/rigel/project/paper_backup/lib/target/debug/deps/liblib-eaabc3bd1e34bc4e.rmeta: src/lib.rs
-
-/home/rigel/project/paper_backup/lib/target/debug/deps/lib-eaabc3bd1e34bc4e.d: src/lib.rs
-
-src/lib.rs:
